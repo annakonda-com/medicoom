afb1231a27351d3baa3baad7ec3839909c61b026
unknown <annakondr0709a@gmail.com>
2025-04-23 19:32:30 +0300

    qqq

--- app/src/main/java/com/medicoom/fragments/ChangeMedicineFragment.java
+++ app/src/main/java/com/medicoom/fragments/ChangeMedicineFragment.java
@@ -12,18 +12,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.medicoom.R;
+import com.medicoom.javaClasses.Medicine;
 import com.medicoom.javaClasses.MedicinePost;
+import com.medicoom.utils.myUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class ChangeMedicineFragment extends BaseFragment {
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("d.MM.yyyy", Locale.getDefault());
+    public int startDate = -1;
+    public int int_num_of_tablets = -1;
+    public int int_remind_when = -1;
 
     private String name;
     private String dosage;
@@ -39,7 +53,6 @@ public class ChangeMedicineFragment extends BaseFragment {
     private final String REMIND_WHEN = "remind_when";
     private final String POST_ID = "post_id";
 
-    SimpleDateFormat dateFormat = new SimpleDateFormat("d.MM.yyyy", Locale.getDefault());
 
     public ChangeMedicineFragment() {
         // Required empty public constructor
@@ -98,7 +111,69 @@ public class ChangeMedicineFragment extends BaseFragment {
             etr.setText(String.valueOf(remind_when));
         }
 
+        Button save = view.findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText name, dosage;
+                name = view.findViewById(R.id.name);
+                dosage = view.findViewById(R.id.dosage);
+                if (validateForm(view)) {
+                    Medicine newPost = new Medicine(name.getText().toString(),
+                            dosage.getText().toString(), int_num_of_tablets,
+                            startDate, int_remind_when);
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance
+                                    ("https://medicoom-abc-default-rtdb.europe-west1.firebasedatabase.app/")
+                            .getReference("users");
+                    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                    mDatabase.child(currentUser.getUid()).child("medicines").child(post_id)
+                            .setValue(newPost).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(getActivity(), "Успешно обновлено!", Toast.LENGTH_LONG).show();
+                                    selfKill();
+                                }
+                            });
+                }
+
+            }
+        });
+
     }
+
+    boolean validateForm(View view) {
+        boolean valid = true;
+        EditText name, dosage, num_of_tablets, remind_when;
+        name = view.findViewById(R.id.name);
+        dosage = view.findViewById(R.id.dosage);
+        num_of_tablets = view.findViewById(R.id.num_of_tablets);
+        remind_when = view.findViewById(R.id.min_tablets);
+        if (name.getText().toString().isEmpty()) {
+            name.setError("Обязательное поле");
+            valid = false;
+        } else {
+            if (myUtils.isSpace(name.getText().toString())) {
+                name.setError("Поле пустое");
+                valid = false;
+            } else {
+                name.setError(null);
+            }
+        }
+        if (!dosage.getText().toString().isEmpty() && myUtils.isSpace(name.getText().toString())) {
+            dosage.setError("Поле пустое");
+            valid = false;
+        } else {
+            dosage.setError(null);
+        }
+        if (!num_of_tablets.getText().toString().isEmpty() && !myUtils.isSpace(remind_when.getText().toString())) {
+            try {
+                int_num_of_tablets = Integer.parseInt(num_of_tablets.getText().toString());
+                num_of_tablets.setError(null);
+            } catch (NumberFormatException e) {
+                num_of_tablets.setError("Введите число!");
+                valid = false;
+            }
+        }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
