ca7e449547b6cbb96b98314d10ab72062c31f538
Anna <annakondr0709a@gmail.com>
2025-04-28 22:06:26 +0300

    appointments push into db beta

--- app/src/main/java/com/medicoom/fragments/InputAppointmentFragment.java
+++ app/src/main/java/com/medicoom/fragments/InputAppointmentFragment.java
@@ -19,16 +19,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -37,14 +43,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.medicoom.R;
+import com.medicoom.javaClasses.Appointment;
 import com.medicoom.javaClasses.Medicine;
 import com.medicoom.javaClasses.MedicinePost;
 import com.medicoom.javaClasses.MedicineSpinnerAdapter;
 import com.medicoom.utils.myUtils;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -59,7 +69,9 @@ public class InputAppointmentFragment extends Fragment {
     SimpleDateFormat timeFormat = new SimpleDateFormat("H:mm", Locale.getDefault());
 
     MedicinePost picked_med;
+    String how_to_get_str = null;
 
+    int num_of_times = 1;
 
 
     public InputAppointmentFragment() {
@@ -67,11 +79,17 @@ public class InputAppointmentFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        RadioButton not_forever;
+        RadioButton by_week;
+        RadioButton at_some_of_days;
+        RadioButton personal;
 
         view.findViewById(R.id.input_date_layout).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,6 +120,8 @@ public class InputAppointmentFragment extends Fragment {
                 if (times != null) {
                     times.clear();
                 }
+                num_of_times = position + 1;
+
                 LinearLayout container = view.findViewById(R.id.times_inputs_container);
                 for (int i = 0; i < container.getChildCount(); i++) {
                     container.getChildAt(i).setVisibility(View.GONE);
@@ -121,7 +141,7 @@ public class InputAppointmentFragment extends Fragment {
         });
 
 
-        RadioButton not_forever = view.findViewById(R.id.not_forever);
+        not_forever = view.findViewById(R.id.not_forever);
         not_forever.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -136,7 +156,7 @@ public class InputAppointmentFragment extends Fragment {
             }
         });
 
-        RadioButton by_week = view.findViewById(R.id.by_week);
+        by_week = view.findViewById(R.id.by_week);
         by_week.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -150,7 +170,7 @@ public class InputAppointmentFragment extends Fragment {
                 }
             }
         });
-        RadioButton at_some_of_days = view.findViewById(R.id.at_some_of_days);
+        at_some_of_days = view.findViewById(R.id.at_some_of_days);
         at_some_of_days.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -164,7 +184,7 @@ public class InputAppointmentFragment extends Fragment {
                 }
             }
         });
-        RadioButton personal = view.findViewById(R.id.personal);
+        personal = view.findViewById(R.id.personal);
         personal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -178,6 +198,15 @@ public class InputAppointmentFragment extends Fragment {
                 }
             }
         });
+        view.findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Appointment new_post = makePost(view);
+                if (makePost(view) != null){
+                    writePost(new_post);
+                }
+            }
+        });
     }
 
     @Override
@@ -186,6 +215,7 @@ public class InputAppointmentFragment extends Fragment {
         View contentView = inflater.inflate(R.layout.fragment_input_appointment, container, false);
         ((RadioButton) contentView.findViewById(R.id.forever)).setChecked(true);
         ((RadioButton) contentView.findViewById(R.id.every_day)).setChecked(true);
+        ((RadioButton) contentView.findViewById(R.id.before)).setChecked(true);
 
         ArrayList<MedicinePost> med_list = new ArrayList<>();
         MedicineSpinnerAdapter adapter = new MedicineSpinnerAdapter(getActivity(), android.R.layout.simple_spinner_item, med_list);
@@ -253,7 +283,6 @@ public class InputAppointmentFragment extends Fragment {
         how_much_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ((Spinner) contentView.findViewById(R.id.how_much_a_day)).setAdapter(how_much_adapter);
 
-
         return contentView;
     }
 
@@ -292,12 +321,101 @@ public class InputAppointmentFragment extends Fragment {
 
     }
 
-    void makePost(View view) {
-
+    void writePost(Appointment post) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance
+                        ("https://medicoom-abc-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("users");
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase.child(currentUser.getUid()).child("appointments").push()
+                .setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getActivity(), "Успешно добавлено!", Toast.LENGTH_LONG).show();
+                        getActivity().getSupportFragmentManager().popBackStack();
+                    }
+                });
     }
-    boolean validateForm(View view){
+
+    Appointment makePost(View view) {
+        if (validateForm(view)) {
+            String medicine_id = picked_med.getPostId();
+
+            if (how_to_get_str == null) {
+                RadioButton checked_rb = view.findViewById(((RadioGroup) view.findViewById(R.id.how_get_layout))
+                        .getCheckedRadioButtonId());
+                how_to_get_str = checked_rb.getText().toString();
+            }
+
+            int amount_at_once = 0;
+            String am_at_once = ((EditText) view.findViewById(R.id.num_of_tablets)).getText().toString();
+            if (!am_at_once.isEmpty()) {
+                amount_at_once = Integer.parseInt(am_at_once);
+            }
+
+            if (startDate == -1) {
+                Calendar loc_startDate = Calendar.getInstance();
+                loc_startDate.set(loc_startDate.get(Calendar.YEAR),
+                        loc_startDate.get(Calendar.MONTH),
+                        loc_startDate.get(Calendar.DAY_OF_MONTH),
+                        0, 0, 0);
+                startDate = (int) (loc_startDate.getTimeInMillis() / 1000L);
+            }
+
+            int overall_duration = -1;
+            String ovll_duration = ((EditText) view.findViewById(R.id.overall_duration)).getText().toString();
+            if (!ovll_duration.isEmpty()) {
+                overall_duration = Integer.parseInt(ovll_duration);
+            }
+
+            List<Integer> week_days_arr = null;
+            Integer[] raw_days = new Integer[8];
+            ChipGroup days_of_week = view.findViewById(R.id.days_of_week);
+            List<Integer> selectedIds = days_of_week.getCheckedChipIds();
+            if (!selectedIds.isEmpty()) {
+                for (int i = 0; i < selectedIds.size(); i++) {
+                    if (selectedIds.get(i) == R.id.monday) {
+                        raw_days[i] = 0;
+                    } else if (selectedIds.get(i) == R.id.tuesday) {
+                        raw_days[i] = 1;
+                    } else if (selectedIds.get(i) == R.id.wednesday) {
+                        raw_days[i] = 2;
+                    } else if (selectedIds.get(i) == R.id.thursday) {
+                        raw_days[i] = 3;
+                    } else if (selectedIds.get(i) == R.id.friday) {
+                        raw_days[i] = 4;
+                    } else if (selectedIds.get(i) == R.id.saturday) {
+                        raw_days[i] = 5;
+                    } else if (selectedIds.get(i) == R.id.sunday) {
+                        raw_days[i] = 6;
+                    }
+                }
+                week_days_arr = Arrays.asList(raw_days);
+            }
+
+            List<Integer> final_times = times;
+
+            int every_x_days = -1;
+            String ddays = ((EditText) view.findViewById(R.id.for_how_much_days)).getText().toString();
+            if (!ddays.isEmpty()) {
+                every_x_days = Integer.parseInt(ddays);
+            }
+            boolean notification = ((CheckBox) view.findViewById(R.id.send_notific)).isChecked();
+            boolean on_pause = false;
+            boolean archive = false;
+            boolean deleted = false;
+
+            Appointment new_post = new Appointment(amount_at_once, archive,
+                    overall_duration, week_days_arr, deleted, every_x_days, how_to_get_str,
+                    medicine_id, notification, on_pause, startDate, final_times);
+            return new_post;
+        }
+        return null;
+    }
+
+    boolean validateForm(View view) {
+
         boolean valid = true;
-        if (((RadioButton) view.findViewById(R.id.not_forever)).isChecked()){
+        if (((RadioButton) view.findViewById(R.id.not_forever)).isChecked()) {
             EditText overall_duration = view.findViewById(R.id.overall_duration);
             if (overall_duration.getText().toString().isEmpty()) {
                 overall_duration.setError("Обязательное поле");
@@ -311,10 +429,42 @@ public class InputAppointmentFragment extends Fragment {
                 }
             }
         }
-        if (((RadioButton) view.findViewById(R.id.by_week)).isChecked()){
-
+        if (((RadioButton) view.findViewById(R.id.by_week)).isChecked()) {
+            ChipGroup days_of_week = view.findViewById(R.id.days_of_week);
+            if (days_of_week.getCheckedChipIds().isEmpty()) {
+                Toast.makeText(getContext(), "Выберите дни недели!", Toast.LENGTH_LONG).show();
+                valid = false;
+            }
+        }
+        if (((RadioButton) view.findViewById(R.id.at_some_of_days)).isChecked()) {
+            EditText for_how_much_days = view.findViewById(R.id.for_how_much_days);
+            if (for_how_much_days.getText().toString().isEmpty()) {
+                for_how_much_days.setError("Введите количество дней");
+                valid = false;
+            } else if (myUtils.isSpace(for_how_much_days.getText().toString())) {
+                for_how_much_days.setError("Поле пустое");
+                valid = false;
+            } else {
+                for_how_much_days.setError(null);
+            }
+        }
+        if (((RadioButton) view.findViewById(R.id.personal)).isChecked()) {
+            EditText how_to_get = view.findViewById(R.id.how_to_get);
+            if (how_to_get.getText().toString().isEmpty()) {
+                how_to_get.setError("Введите как принимать");
+                valid = false;
+            } else if (myUtils.isSpace(how_to_get.getText().toString())) {
+                how_to_get.setError("Поле пустое");
+                valid = false;
+            } else {
+                how_to_get_str = how_to_get.getText().toString();
+                how_to_get.setError(null);
+            }
         }
 
-        if
+        while (times.size() < num_of_times) {
+            times.add(-1);
+        }
+        return valid;
     }
 }
---
+++ app/src/main/java/com/medicoom/javaClasses/Appointment.java
@@ -0,0 +1,134 @@
+package com.medicoom.javaClasses;
+
+import java.util.List;
+
+public class Appointment {
+    String medicine_id;
+    String how_to_get;
+    int amount_at_once;
+    int start_date;
+    int days;
+    List<Integer> days_of_week;
+    List<Integer> times;
+    int every_x_days;
+    boolean notifications;
+    boolean on_pause;
+    boolean archive;
+    boolean deleted;
+
+    public Appointment(int amount_at_once, boolean archive, int days, List<Integer> days_of_week,
+                       boolean deleted, int every_x_days, String how_to_get, String medicine_id,
+                       boolean notifications, boolean on_pause, int start_date, List<Integer> times) {
+        this.amount_at_once = amount_at_once;
+        this.archive = archive;
+        this.days = days;
+        this.days_of_week = days_of_week;
+        this.deleted = deleted;
+        this.every_x_days = every_x_days;
+        this.how_to_get = how_to_get;
+        this.medicine_id = medicine_id;
+        this.notifications = notifications;
+        this.on_pause = on_pause;
+        this.start_date = start_date;
+        this.times = times;
+    }
+    public Appointment(){
+
+    }
+
+    public int getAmount_at_once() {
+        return amount_at_once;
+    }
+
+    public void setAmount_at_once(int amount_at_once) {
+        this.amount_at_once = amount_at_once;
+    }
+
+    public boolean isArchive() {
+        return archive;
+    }
+
+    public void setArchive(boolean archive) {
+        this.archive = archive;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public List<Integer> getDays_of_week() {
+        return days_of_week;
+    }
+
+    public void setDays_of_week(List<Integer> days_of_week) {
+        this.days_of_week = days_of_week;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public int getEvery_x_days() {
+        return every_x_days;
+    }
+
+    public void setEvery_x_days(int every_x_days) {
+        this.every_x_days = every_x_days;
+    }
+
+    public String getHow_to_get() {
+        return how_to_get;
+    }
+
+    public void setHow_to_get(String how_to_get) {
+        this.how_to_get = how_to_get;
+    }
+
+    public String getMedicine_id() {
+        return medicine_id;
+    }
+
+    public void setMedicine_id(String medicine_id) {
+        this.medicine_id = medicine_id;
+    }
+
+    public boolean isNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(boolean notifications) {
+        this.notifications = notifications;
+    }
+
+    public boolean isOn_pause() {
+        return on_pause;
+    }
+
+    public void setOn_pause(boolean on_pause) {
+        this.on_pause = on_pause;
+    }
+
+    public int getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(int start_date) {
+        this.start_date = start_date;
+    }
+
+    public List<Integer> getTimes() {
+        return times;
+    }
+
+    public void setTimes(List<Integer> times) {
+        this.times = times;
+    }
+}
